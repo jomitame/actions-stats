8c1d6341e6e1a38cd5517dd9a4d9218216f249e4
jomitame <jomitame@gmail.com>
2025-04-09 13:04:35 -0500

    feat: :tada: Project Creation
    All files to create github actions

---
+++ README.md
@@ -0,0 +1,21 @@
+# Stats Project
+
+This project generates a json file with stats about the numbers of files of some problems by users.
+
+Is a clone of the project "Retos de programacion" by [MoureDev](https://www.youtube.com/watch?v=pNtcTmCiXzw)
+
+This project was created to learn how to work with github actions.
+
+There are two actions:
+* One action executes with every push (SP)
+* Other action executes with a cron (SC)
+
+## Download
+```sh
+git clone git@github.com:jomitame/actions-stats.git
+cd actions-stats
+```
+## Execute
+```python
+python stats.py
+```
---
+++ stats.py
@@ -0,0 +1,146 @@
+import os
+import re
+import json
+
+# pattern start with two digit then an space a hyphen then another space
+CHALLENGE_PATTERN = r"^\d{2} - "
+
+EXCLUDED = ["stats.py", "stats.json", "ejercicio.md",
+            ".ds_store", "tempcoderunnerfile.py"]
+
+# the path wher this file is located
+path = os.path.dirname(__file__)
+
+# variables initialization
+challenges = {}
+current_challenge = ""
+languages = {}
+users = {}
+files_total = 0
+
+# print(next(os.walk(path))) # to watch the content os walk function
+
+# looping throug this folder
+for dirpath, _, files in os.walk(path):
+
+    # get the name of directory where this file is in
+    challenge = os.path.basename(dirpath)
+
+    # evaluates if this directoy start with the patern
+    if re.match(CHALLENGE_PATTERN, challenge):
+        
+        # change the value of current to the name of the dirctory 
+        current_challenge = challenge
+        
+    language_files = 0
+
+    for file in files:
+        if file.lower() not in EXCLUDED:
+            language = os.path.basename(dirpath).lower() # the same is -> challenge.lower()
+            languages[language] = languages.get(language, 0) + 1 # adds 1 to the value of language key
+
+            # TODO: continue commenting this code
+            user = os.path.splitext(file)[0].lower()
+            print(os.path.splitext(file))
+            user_files = users.get(user, (0, []))[0] + 1
+            user_languages = users.get(user, (0, []))[1]
+
+            if language not in user_languages:
+                user_languages.append(language)
+
+            users[user] = (user_files, user_languages)
+
+            language_files += 1
+            files_total += 1
+
+    if current_challenge != "":
+        challenges[current_challenge] = challenges.get(
+            current_challenge, 0) + language_files
+
+sorted_challenges = sorted(
+    challenges.items(), key=lambda challenge: challenge[1], reverse=True)
+
+sorted_languages = sorted(
+    languages.items(), key=lambda language: language[1], reverse=True)
+
+sorted_users = sorted(
+    users.items(), key=lambda user: (user[1][0], len(user[1][1])), reverse=True)
+
+challenges_total = len(challenges)
+languages_total = len(languages)
+users_total = len(users)
+
+challenges_ranking = []
+languages_ranking = []
+users_ranking = []
+
+print("ESTADÍSTICAS:\n")
+
+print(f"Retos: {challenges_total}")
+print(f"Lenguajes de programación: {languages_total}")
+print(f"Correcciones: {files_total}")
+print(f"Usuarios: {users_total}")
+
+print("\nRanking de retos:")
+for index, challenge in enumerate(sorted_challenges):
+
+    order = index + 1
+    name = challenge[0]
+    count = challenge[1]
+
+    challenges_ranking.append({
+        "order": order,
+        "name": name,
+        "count": count
+    })
+
+    print(f"#{order} {name} (Ejercicios: {count})")
+
+print("\nRanking de lenguajes:")
+for index, language in enumerate(sorted_languages):
+
+    order = index + 1
+    name = language[0]
+    count = language[1]
+    percentage = round((language[1] * 100) / files_total, 2)
+
+    languages_ranking.append({
+        "order": order,
+        "name": name,
+        "count": count,
+        "percentage": percentage
+    })
+
+    print(f"#{order} {name} (Ejercicios: {count}) (Porcentaje: {percentage}%)")
+
+print("\nRanking de usuarios:")
+for index, user in enumerate(sorted_users):
+
+    order = index + 1
+    name = user[0]
+    count = user[1][0]
+    languages = len(user[1][1])
+
+    users_ranking.append({
+        "order": order,
+        "name": name,
+        "count": count,
+        "languages": languages
+    })
+
+    print(
+        f"#{order} {name} (Ejercicios: {count}) (Lenguajes: {languages})")
+
+data = {
+    "challenges_total": challenges_total,
+    "languages_total": languages_total,
+    "files_total": files_total,
+    "users_total": users_total,
+    "challenges_ranking": challenges_ranking,
+    "languages_ranking": languages_ranking,
+    "users_ranking": users_ranking
+}
+
+json_data = json.dumps(data, indent=4)
+with open(os.path.join(path, "stats.json"), "w") as file:
+    file.write(json_data)
